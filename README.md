91d17c8f4a6ee57bc7393fdcf8d7de72336183e0
Thomas Fuchs <thomas@fesch.at>
2013-08-21 08:14:54 -0400

    zprogress was made

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
 zprogress
 =========
 
-Thin-line progress bar for Zepto
+Thin-line progress bar with fake trickling for Zepto. You know, like iOS [redacted].
+
+Heavily inspired by https://github.com/rstacruz/nprogress, which is awesome
+but requires jQuery. By making use of Zepto's CSS-based animation
+(and webkit-only stuff), zprogress is leaner (but won't work on as
+many devices, so YMMV).
+
+Otherwise, not regarding CSS, zprogress is a drop-in
+replacement for nprogress.
+
+To use it, just include zprogress.js. There's no CSS file.
+
+```javascript
+// show progress bar
+$.zprogress.start()
+
+// ok, you're done
+$.zprogress.done()
+
+// set to percentage (0..1)
+$.zprogress.set(0.4)
+
+// increase a bit
+$.zprogress.inc()
+
+// set color of progress bar
+$.zprogress.color('gold')
+```
+
+Note that zprogress won't work with jQuery because it relies on the features of
+[Zepto's animate method](http://zeptojs.com/#animate).
+
+zprogress is licensed under the terms of the MIT license.
---
+++ zprogress.js
@@ -0,0 +1,69 @@
+// zprogress (c) 2013 Thomas Fuchs
+// MIT-licensed - https://github.com/madrobby/zprogress
+
+;(function($){
+  var initialized = false,
+    html =
+      '<style>#zprogress{position:fixed;top:0;left:0;width:100%;height:3px;opacity:0;pointer-events:none;-webkit-pointer-events:none;z-index:1000}'+
+      '#zprogress_indicator{width:100%;margin-left:-100%;height:100%;background:#1c88ff}'+
+      '</style>'+
+      '<div id=zprogress><div id=zprogress_indicator></div></div>',
+    $wrapper, $indicator, value, timeout
+
+  function init(){
+    if(initialized) return
+
+    $('body').append(html)
+    $wrapper = $('#zprogress')
+    $indicator = $('#zprogress_indicator')
+  }
+
+  function anim(){
+    $indicator.animate({ translateX: value*100+'%' }, 200)
+  }
+
+  function clear(){
+    if(timeout) clearInterval(timeout)
+    timeout = undefined
+  }
+
+  function trickle(){
+    setTimeout(function(){
+      $.zprogress.inc((0.85-value)*.035*Math.random())
+      trickle()
+    }, 350+(400*Math.random()))
+  }
+
+  $.zprogress = {
+    start: function(){
+      init()
+      clear()
+      value = .1
+      $wrapper.animate({ opacity: 1 })
+      $indicator.animate({ translateX: '0%' }, 0)
+      anim()
+      trickle()
+    },
+    inc: function(delta){
+      if(value<.85) value+=delta||.05
+      anim()
+    },
+    set: function(newValue){
+      clear()
+      value = newValue
+      anim()
+      trickle()
+    },
+    done: function(){
+      clear()
+      init()
+      value = 1
+      anim()
+      setTimeout(function(){$wrapper.animate({ opacity: 0 })}, 100)
+    },
+    color: function(color){
+      init()
+      $indicator.css({ backgroundColor: color })
+    }
+  }
+})(Zepto)
